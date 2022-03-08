bb364efb8bde9bb6d80f5ef021639f76633b8b68
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2022-03-08 14:08:13 +0000

    AST-9953 Add GroupBy and State enum

---
+++ ast-visual-studio-extension/CxExtension/Enums/GroupBy.cs
@@ -0,0 +1,11 @@
+﻿namespace ast_visual_studio_extension.CxExtension.Enums
+{
+    public enum GroupBy
+    {
+        ENGINE,
+        FILE,
+        SEVERITY,
+        STATE,
+        QUERY_NAME
+    }
+}
