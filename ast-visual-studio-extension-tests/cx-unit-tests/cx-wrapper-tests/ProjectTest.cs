4062708cb3b2b64902a87bb4faf50f8bbc35d379
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2022-04-13 09:55:59 +0100

    remove scan create test

--- ast-visual-studio-extension-tests/cx-unit-tests/cx-wrapper-tests/ProjectTest.cs
+++ ast-visual-studio-extension-tests/cx-unit-tests/cx-wrapper-tests/ProjectTest.cs
@@ -23,17 +23,5 @@ namespace ast_visual_studio_extension_tests.cx_unit_tests.cx_wrapper_tests
             List<Project> projectList = cxWrapper.GetProjects("limit=10");
             Assert.True(projectList.Count <= 10);
         }
-
-        [Fact]
-        public void TestProjectBranches()
-        {
-            Dictionary<string, string> parameters = GetCommonParams();
-            parameters["--branch"] =  "test";
-
-            Scan scan = cxWrapper.ScanCreate(parameters);
-            List<string> branches = cxWrapper.GetBranches(scan.ProjectId);
-            Assert.True(branches.Any());
-            Assert.Contains("test", branches);
-        }
     }
 }
