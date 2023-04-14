a5db0582ade014ffce9ce5b0c8effa3e2b8c5175
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2023-04-14 11:37:55 +0100

    test

--- ast-visual-studio-extension/CxExtension/CxWindowPackage.cs
+++ ast-visual-studio-extension/CxExtension/CxWindowPackage.cs
@@ -50,9 +50,11 @@ namespace ast_visual_studio_extension.CxExtension
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            await base.InitializeAsync(cancellationToken, progress);
+
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync();
 
             // Command to create Checkmarx extension main window
             await CxWindowCommand.InitializeAsync(this);
