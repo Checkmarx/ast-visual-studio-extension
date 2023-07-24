15ab18c755a7ed11404e55f0e812989ff56d9124
igorlombacx <igor.lomba@checkmarx.com>
2023-07-24 17:21:20 +0100

    refactoring ParseAdditionalParameters to CxUtils

---
+++ ast-visual-studio-extension/CxWrapper/CxUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ast_visual_studio_extension.CxCLI
+{
+    public class CxUtils
+    {
+        public static List<string> ParseAdditionalParameters(String additionalParameters)
+        {
+            List<string> additionalParametersList = new List<string>();
+            if (!string.IsNullOrEmpty(additionalParameters))
+            {
+                // regex to exclude spaces and search for "" and ''
+                foreach (Match match in Regex.Matches(additionalParameters, "(?:[^\\s\"']+|\"[^\"]*\"|'[^\']*')+", RegexOptions.IgnoreCase))
+                {
+                    additionalParametersList.Add(match.Captures[0].Value.Replace("\'", "\""));
+                }
+            }
+            return additionalParametersList;
+        }
+    }
+}
