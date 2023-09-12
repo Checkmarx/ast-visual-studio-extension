0a82056ea16c78794a189d7f6007e41350468c2f
igorlombacx <igor.lomba@checkmarx.com>
2023-09-12 15:07:31 +0100

    ClearPanels changes

--- ast-visual-studio-extension/CxExtension/Panels/ResultsTreePanel.cs
+++ ast-visual-studio-extension/CxExtension/Panels/ResultsTreePanel.cs
@@ -167,7 +167,6 @@ namespace ast_visual_studio_extension.CxExtension.Panels
         private void ClearPanels(bool clearSecondAndThirdPanel)
         {
             cxWindowUI.TreeViewResults.Items.Clear();
-            cxWindowUI.VulnerabilitiesPanel.Visibility = Visibility.Hidden;
             if (clearSecondAndThirdPanel)
             {
                 resultInfoPanel.Clear();
