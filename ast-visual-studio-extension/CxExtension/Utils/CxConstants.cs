326abf6a4a2cb34b56b23fafd6e1204b3dea608c
igorlombacx <igor.lomba@checkmarx.com>
2023-10-13 15:30:27 +0100

    updating notify message

--- ast-visual-studio-extension/CxExtension/Utils/CxConstants.cs
+++ ast-visual-studio-extension/CxExtension/Utils/CxConstants.cs
@@ -45,7 +45,7 @@
         /************ NOTIFICATIONS ************/
         public static string NOTIFY_FILE_NOT_FOUND => "File {0} not found in the solution. Please ensure you are in the correct scan.";
 
-        public static string NOTIFY_SOLUTION_NOT_FOUND => "Unable to find source in project. Please ensure you have an open project or solution. If you don't have an existing project, you can create a new one using 'File > New > Project'.";
+        public static string NOTIFY_SOLUTION_NOT_FOUND => "Unable to find source in project. Please ensure you have an open project or solution.";
 
         /************ TOOLBAR ************/
         public static string TOOLBAR_SELECT_PROJECT => "Select a project";
