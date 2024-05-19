0d289ad20873081e96aa03eab8de4c4fe11dd463
tamarleviCm <tamar.levi@checkmarx.com>
2024-05-19 10:35:39 +0300

    Update CxWrapper.cs

--- ast-visual-studio-extension/CxWrapper/CxWrapper.cs
+++ ast-visual-studio-extension/CxWrapper/CxWrapper.cs
@@ -95,7 +95,7 @@ namespace ast_visual_studio_extension.CxCLI
                 CxConstants.FLAG_SCAN_ID, scanId.ToString(),
                 CxConstants.FLAG_REPORT_FORMAT, reportFormat.ToString(),
                 CxConstants.FLAG_OUTPUT_NAME, fileName,
-                CxConstants.FLAG_OUTPUT_PATH, tempDir
+                CxConstants.FLAG_OUTPUT_PATH, tempDir,
                 CxConstants.FLAG_AGENT, CxCLI.CxConstants.EXTENSION_AGENT
             };
 
