38a5bd40b515eede593f34138ac3b1e51d3ca7dd
elchananarb <elchanan.arbiv@checkmarx.com>
2024-12-17 19:20:58 +0200

    UI automation test infrastructure

---
+++ UITests/BaseTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlaUI.UIA3;
+using FlaUI.Core;
+using System;
+using System.Threading.Tasks;
+using EnvDTE;
+using AutomationWindow = FlaUI.Core.AutomationElements.Window;
+
+namespace UITests
+{
+   [TestClass]
+   public class BaseTest
+   {
+       protected static UIA3Automation _automation;
+       protected static Application _app;
+       protected static AutomationWindow _mainWindow;
+       protected static DTE _dte;
+
+       [ClassInitialize]
+       public static void ClassInitialize(TestContext context)
+       {
+           // Initialize automation and launch VS
+           _automation = new UIA3Automation();
+           _app = Application.Launch("devenv.exe");
+
+           // Wait until the main window is available
+           _mainWindow = WaitForMainWindow();
+
+           // Handle initial setup
+           SetupVisualStudio().Wait();
+       }
+
+       private static async Task SetupVisualStudio()
+       {
+           await Task.Delay(5000);
+
+           var continueWithoutCodeButton = _mainWindow.FindFirstDescendant(cf => cf.ByName("Continue without code"));
+           if (continueWithoutCodeButton != null)
+           {
+               var invokePattern = continueWithoutCodeButton.Patterns.Invoke.Pattern;
+               invokePattern.Invoke();
+               await Task.Delay(2000);
+               _mainWindow = WaitForMainWindow();
+           }
+       }
+
+       public static AutomationWindow WaitForMainWindow(int timeoutInSeconds = 30)
+       {
+           var startTime = DateTime.Now;
+           while ((DateTime.Now - startTime).TotalSeconds < timeoutInSeconds)
+           {
+               try
+               {
+                   Task.Delay(10000).Wait();
+                   var window = _app.GetMainWindow(_automation);
+                   if (window != null && window.IsAvailable)
+                   {
+                       return window;
+                   }
+               }
+               catch (Exception)
+               {
+                   // Ignore exception, retry until timeout
+               }
+               Task.Delay(500).Wait();
+           }
+           throw new TimeoutException("Main window did not appear within the timeout period.");
+       }
+
+       [ClassCleanup]
+       public static void ClassCleanup()
+       {
+           try
+           {
+               if (_app != null)
+               {
+                   _app.Close();
+                   _app.Dispose();
+               }
+           }
+           finally
+           {
+               if (_automation != null)
+               {
+                   _automation.Dispose();
+               }
+               _mainWindow = null;
+           }
+       }
+   }
+}
---
+++ UITests/CheckmarxTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlaUI.Core.AutomationElements;
+using System.Threading.Tasks;
+
+namespace UITests
+{
+    [TestClass]
+    public class CheckmarxTests : BaseTest
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            ClassInitialize(context);
+        }
+
+        [TestMethod]
+        public async Task OpenCheckmarxWindow()
+        {
+            var viewMenu = _mainWindow.FindFirstDescendant(cf => cf.ByName("View"));
+            if (viewMenu != null)
+            {
+                viewMenu.WaitUntilEnabled().Click();
+                await Task.Delay(1000);
+
+                var allMenuItems = _mainWindow.FindAllDescendants(cf =>
+                    cf.ByControlType(FlaUI.Core.Definitions.ControlType.MenuItem));
+
+                foreach (var menuItem in allMenuItems)
+                {
+                    if (menuItem.Name == "Other Windows")
+                    {
+                        menuItem.WaitUntilEnabled().Click();
+                        await Task.Delay(2000);
+
+                        var checkmarxOption = _mainWindow.FindFirstDescendant(cf => cf.ByName("Checkmarx"));
+                        if (checkmarxOption != null)
+                        {
+                            checkmarxOption.WaitUntilEnabled().Click();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
