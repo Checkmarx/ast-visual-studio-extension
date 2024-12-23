30bdafdf7c34830012627bcf28b7ccb752abd33a
elchananarb <elchanan.arbiv@checkmarx.com>
2024-12-23 20:14:49 +0200

    Load the extension in the background without affecting the UI

--- ast-visual-studio-extension/CxExtension/Commands/AddExtensionWindowCmd.cs
+++ ast-visual-studio-extension/CxExtension/Commands/AddExtensionWindowCmd.cs
@@ -52,19 +52,18 @@ namespace ast_visual_studio_extension.CxExtension.Commands
                 {
                     // Adds a 5-second delay to allow background processes initialization to complete before proceeding.
                     await Task.Delay(5000);
-                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                    ToolWindowPane window = await this.package.ShowToolWindowAsync(typeof(CxWindow), 0, true, this.package.DisposalToken);
+
+                    // Load the tool window in the background without affecting the UI
+                    ToolWindowPane window = await this.package.FindToolWindowAsync(typeof(CxWindow), 0, create: true, this.package.DisposalToken);
                     if ((null == window) || (null == window.Frame))
                     {
                         throw new NotSupportedException("Cannot create tool window");
                     }
-                    // Explicitly show the window
-                    var frame = (IVsWindowFrame)window.Frame;
-                    Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(frame.ShowNoActivate());
+
                 }
                 catch (Exception ex)
                 {
-                    Debug.WriteLine($"Failed to show window: {ex}");
+                    Debug.WriteLine($"Failed to load window in the background: {ex}");
                 }
             });
         }
