e7571ab2e8f4c1a83dc8aa9efb273a13fc695c96
elchananarb <elchanan.arbiv@checkmarx.com>
2025-01-07 09:33:42 +0200

    Update CheckmarxTests.cs

--- UITests/CheckmarxTests.cs
+++ UITests/CheckmarxTests.cs
@@ -18,35 +18,34 @@ namespace UITests
         {
             // Find the View menu
             var viewMenu = _mainWindow.FindFirstDescendant(cf => cf.ByName("View"));
-            if (viewMenu != null)
+            Assert.IsNotNull(viewMenu, "View menu not found");
+
+            // Open the "View" menu by clicking it
+            viewMenu.WaitUntilEnabled().Click();
+            await Task.Delay(500);
+
+            var allMenuItems = _mainWindow.FindAllDescendants(cf =>
+                cf.ByControlType(FlaUI.Core.Definitions.ControlType.MenuItem));
+            bool foundOtherWindows = false;
+
+
+            foreach (var menuItem in allMenuItems)
             {
-                // Open the "View" menu by clicking it
-                viewMenu.WaitUntilEnabled().Click();
-                await Task.Delay(500);
-
-                var allMenuItems = _mainWindow.FindAllDescendants(cf =>
-                    cf.ByControlType(FlaUI.Core.Definitions.ControlType.MenuItem));
-                bool foundOtherWindows = false;
-
-
-                foreach (var menuItem in allMenuItems)
+                if (menuItem.Name == "Other Windows")
                 {
-                    if (menuItem.Name == "Other Windows")
-                    {
-                        foundOtherWindows = true;
-                        menuItem.WaitUntilEnabled().Click();
-                        await Task.Delay(1000);
+                    foundOtherWindows = true;
+                    menuItem.WaitUntilEnabled().Click();
+                    await Task.Delay(1000);
 
-                        // Now select a specific window from the list "Checkmarx"
-                        var checkmarxOption = _mainWindow.FindFirstDescendant(cf => cf.ByName("Checkmarx"));
-                        Assert.IsNotNull(checkmarxOption, "Checkmarx option not found in Other Windows menu");
-                        checkmarxOption.WaitUntilEnabled().Click();
-                        break;
-                    }
+                    // Now select a specific window from the list "Checkmarx"
+                    var checkmarxOption = _mainWindow.FindFirstDescendant(cf => cf.ByName("Checkmarx"));
+                    Assert.IsNotNull(checkmarxOption, "Checkmarx option not found in Other Windows menu");
+                    checkmarxOption.WaitUntilEnabled().Click();
+                    break;
                 }
-                Assert.IsTrue(foundOtherWindows, "Other Windows menu item not found");
-
             }
+            Assert.IsTrue(foundOtherWindows, "Other Windows menu item not found");
+
         }
     }
 }
